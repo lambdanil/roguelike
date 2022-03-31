5ce19882a274225897cdd46203253e72f22fc81c
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2022-03-31 23:34:50 +0200

    Add files via upload

--- roguelike.cpp
+++ roguelike.cpp
@@ -2,14 +2,14 @@
 #include <random>
 #include <time.h>
 #include <stdio.h>
+#include <vector>
 
-constexpr int WIDTH = 90;
-constexpr int HEIGHT = 35;
-constexpr int MINX = 12;
-constexpr int MAXX = 18;
-constexpr int MINY = 5;
-constexpr int MAXY = 7;
-
+const int WIDTH = 90;
+const int HEIGHT = 35;
+const int MINX = 12;
+const int MINY = 5;
+const int MAXY = 7;
+const int MAXX = 18;
 
 
 void print_grid(char grid[][WIDTH], int height, int width) {
@@ -34,60 +34,114 @@ int main()
         }
     }
     // Rooms
-    int rooms = 10 + (rand() % static_cast<int>(20 - 10 + 1));
+    int rooms = 15 + (rand() % static_cast<int>(20 - 15 + 1));
+    std::vector<int> vector_rooms;
+    bool* found;
+    found = new bool;
+    int* roomaddx;
+    roomaddx = new int;
+    int* roomaddy;
+    roomaddy = new int;
+    int* roomendx;
+    roomendx = new int;
+    int* roomendy;
+    roomendy = new int;
+    int* roomsizex;
+    roomsizex = new int;
+    int* roomsizey;
+    roomsizey = new int;
     for (int i = 0; i <= rooms; i++) {
-        int roomaddx = 0 + (rand() % static_cast<int>(width - 0 + 1)); // Not actually random
-        int roomaddy = 0 + (rand() % static_cast<int>(height - 0 + 1));
-        int roomsizex = MINX + (rand() % static_cast<int>(MAXY - MINX + 1)); // 5 is min, 10 is max
-        int roomsizey = MINY + (rand() % static_cast<int>(MAXY - MINY + 1));
-        int roomendx = roomaddx+roomsizex;
-        int roomendy = roomaddy+roomsizey;
-        roomaddx += 2;
-        roomaddy += 2;
+        *roomaddx = 0 + (rand() % static_cast<int>(width - 0 + 1)); // Not actually random
+        *roomaddy = 0 + (rand() % static_cast<int>(height - 0 + 1));
+        *roomsizex = MINX + (rand() % static_cast<int>(MAXY - MINX + 1)); // 5 is min, 10 is max
+        *roomsizey = MINY + (rand() % static_cast<int>(MAXY - MINY + 1));
+        *roomendx = *roomaddx+*roomsizex;
+        *roomendy = *roomaddy+*roomsizey;
+
+        vector_rooms.push_back(*roomaddy);
+        vector_rooms.push_back(*roomaddx);
+        vector_rooms.push_back(*roomendy);
+        vector_rooms.push_back(*roomendx);
+
+        *roomaddx += 2;
+        *roomaddy += 2;
         while (true) {
-            if (roomendx <= width-1) {
+            if (*roomendx <= width-1) {
                 break;
             }
-            roomaddx -= 1;
-            roomsizex -= 1;
-            roomendx -= 1;
+            *roomaddx -= 1;
+            *roomsizex -= 1;
+            *roomendx -= 1;
         }
         while (true) {
-            if (roomendy <= height-1) {
+            if (*roomendy <= height-1) {
                 break;
             }
-            roomaddy -= 1;
-            roomsizey -= 1;
-            roomendy -= 1;
+            *roomaddy -= 1;
+            *roomsizey -= 1;
+            *roomendy -= 1;
         }
-        for (int yi = roomaddy; yi < roomendy && yi < height; yi++) {
-            for (int xi = roomaddx; xi < roomendx && xi < width; xi++) {
+        *found = false;
+        for (int yi = *roomaddy; yi < *roomendy && yi < height; yi++) {
+            for (int xi = *roomaddx; xi < *roomendx && xi < width; xi++) {
                 grid[yi][xi] = '#';
-                if (xi == roomaddx && (grid[yi][xi-1] != '#') && (grid[yi][xi-2] != '#')) {
+                if (xi == *roomaddx && (grid[yi][xi-1] != '#') && (grid[yi][xi-2] != '#')) {
                     grid[yi][xi-1] = '|';
+                    if ((rand() % 5) == 1 && not *found) {
+                        *found = true;
+                        grid[yi][xi-1] = '+';
+                    }
                 }
-                if (xi == roomendx-1 && (grid[yi][xi+1] != '#') && (grid[yi][xi+2] != '#')) {
+                if (xi == *roomendx-1 && (grid[yi][xi+1] != '#') && (grid[yi][xi+2] != '#')) {
                     grid[yi][xi+1] = '|';
+                    if ((rand() % 5) == 1 && not *found) {
+                        *found = true;
+                        grid[yi][xi+1] = '+';
+                    }
                 }
-                if (yi == roomaddy && (grid[yi-1][xi] != '#') && (grid[yi-2][xi] != '#')) {
+                if (yi == *roomaddy && (grid[yi-1][xi] != '#') && (grid[yi-2][xi] != '#')) {
                     grid[yi-1][xi] = '=';
+                    if ((rand() % 5) == 1 && not *found) {
+                        *found = true;
+                        grid[yi-1][xi] = '+';
+                    }
                 }
-                if (yi == roomendy-1 && (grid[yi+1][xi] != '#') && (grid[yi+2][xi] != '#')) {
+                if (yi == *roomendy-1 && (grid[yi+1][xi] != '#') && (grid[yi+2][xi] != '#')) {
                     grid[yi+1][xi] = '=';
-                }
-            }
-        }
-        for (int yi = 0; yi < height; yi++) {
-            for (int xi = 0; xi < width; xi++) {
-                if (grid[yi][xi] == '|' && (grid[yi][xi-1] == '#') && (grid[yi][xi+1] == '#')) {
-                    grid [yi][xi] = '#';
-                }
-                if (grid[yi][xi] == '=' && (grid[yi-1][xi] == '#') && (grid[yi+1][xi] == '#')) {
-                    grid [yi][xi] = '#';
+                    if ((rand() % 5) == 1 && not *found) {
+                        *found = true;
+                        grid[yi+1][xi] = '+';
+                    }
                 }
             }
         }
     }
+    int* startpoint;
+    startpoint = new int[2];
+    int* endpoint;
+    endpoint = new int[2];
+    for (int yi = *roomaddy; yi < *roomendy && yi < height; yi++) {
+        for (int xi = *roomaddx; xi < *roomendx && xi < width; xi++) {
+            if (grid[yi][xi] == '+') {
+                startpoint[0] = yi;
+                startpoint[1] = xi;
+                for (int yyi = *roomaddy; yyi < *roomendy && yyi < height; yyi++) {
+                    for (int xxi = *roomaddx; xxi < *roomendx && xxi < width; xxi++) {
+                    }
+                }
+            }
+        }
+    }
+    std::cout << startpoint[0] << std::endl;
+    delete[] endpoint;
+    delete[] startpoint;
+    delete roomaddx;
+    delete roomaddy;
+    delete roomendx;
+    delete roomendy;
+    delete roomsizex;
+    delete roomsizey;
+
     // Generate grid
     print_grid(grid, height, width);
 
